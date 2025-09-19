e8b5c6955c4f7063dcf0b4f9b6aeab2357725a1e
Inam <inamm081@gmail.com>
2025-09-20 00:05:14 +0500

    Basic Setup complete

--- FileUploader/SaveFile/views.py
+++ FileUploader/SaveFile/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def Home(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
