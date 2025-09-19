4d5ee8f4a5ab0c96e111d821a027044a0e7d7214
Inam <inamm081@gmail.com>
2025-09-20 00:30:12 +0500

    Basic File upload setup is completed

--- FileUploader/SaveFile/urls.py
+++ FileUploader/SaveFile/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home, name='Home'), # For index page or default page
+    path('', views.index, name='index'), # For index page or default page
+    path('upload/', views.upload_file, name='upload_file'),
 ]
--- FileUploader/SaveFile/views.py
+++ FileUploader/SaveFile/views.py
@@ -1,6 +1,26 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
-def Home(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+# Debugging 
+import sys
+from pprint import pprint
+
+def dd(var):
+    pprint(var)
+    sys.exit()
+
+def index(request):
+    return render(request, 'index.html')
+
+def upload_file(request):
+    if request.method == 'POST' and request.FILES['file']:
+        file = request.FILES['file']
+
+        # store in media/ folder
+        fs = FileSystemStorage()
+        filename = fs.save(file.name, file)
+        file_url = fs.url(filename)
+
+        return render(request, 'index.html', {'file_url': file_url})
+
+    return render(request, 'index.html')
